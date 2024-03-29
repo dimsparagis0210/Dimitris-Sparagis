3475a3e6c58a7b29872a3ccab95abe9fc929d23d
Dimitris Sparagis <dimsparagis@gmail.com>
2024-03-29 15:32:55 +0200

    Added smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
 'use client';
-
 import {Home} from "@/components/Home/Home";
 import localFont from "next/font/local";
 import About from "@/components/About/About";
 import { Skills } from "@/components/Skills/Skills";
+import {ReactLenis, useLenis} from "@studio-freight/react-lenis";
 const myFont = localFont({
     src: './fonts/CabinetGrotesk-Regular.woff2',
 });
 
 export default function MyApp() {
-
+    const lenis = useLenis(({scroll}) => {
+        //code
+    })
     return (
-        <div className={`${myFont.className}`}>
-            <Home />
-            <About />
-            <Skills />
-        </div>
+        <ReactLenis root>
+            <div className={`${myFont.className}`}>
+                <Home />
+                <About />
+                <Skills />
+            </div>
+        </ReactLenis>
     )
 }
 
