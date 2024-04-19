8da0bb2ab8ea534c01e18e304cc9ea02ea476378
Dimitris Sparagis <dimsparagis@gmail.com>
2024-04-19 21:11:12 +0300

    Extracted hook for text hover animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { Scroll } from '@/components/Layout/Scroll';
+'use client';
+
+import {Scroll} from '@/components/Layout/Scroll';
 import './globals.css';
 import {Circle} from "@/components/Layout/Circle";
 import {ArrowUpward} from "@mui/icons-material";
+import {useTextHoverAnimation} from "@/hooks/useTextHoverAnimation";
 
 export default function MyApp() {
+    const { ref1, ref2, buttonRef } = useTextHoverAnimation();
+
     return (
-        <div className={`bg-myWhite h-screen flex flex-col gap-[10rem]`}>
+        <div className={`bg-myWhite min-h-screen flex flex-col gap-[10rem]`}>
             <header className={`flex`}>
                 <div className={`aspect-square w-[100px] absolute`}>
                     <img src="/blackLogo.png" alt=""/>
@@ -40,12 +45,22 @@ export default function MyApp() {
                 </div>
             </main>
             <section className={`flex justify-center items-center gap-2 text-xl`}>
-                <div className={`rounded-full bg-white aspect-square w-[5rem] 
-                    flex justify-center items-center rotate-45`}>
-                    <ArrowUpward
-                        fontSize={`large`}
-                    />
-                </div>
+                <button className={`rounded-full bg-white aspect-square w-[5rem]
+                 flex justify-center items-center`}
+                        ref={buttonRef}
+                >
+                        <div className={`rotate-45 flex overflow-hidden`}>
+                            <ArrowUpward
+                                fontSize={`large`}
+                                ref={ref1}
+                            />
+                            <ArrowUpward
+                                ref={ref2}
+                                fontSize={`large`}
+                                className={`absolute translate-y-8`}
+                            />
+                        </div>
+                </button>
                 <p>Download or view CV</p>
             </section>
             <footer>
---
+++ hooks/useTextHoverAnimation.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import {useEffect, useRef} from "react";
+import gsap from "gsap";
+
+export const useTextHoverAnimation = () => {
+    const ref1 = useRef(null);
+    const ref2 = useRef(null);
+    const buttonRef = useRef(null);
+
+    useEffect(() => {
+        const handleMouseEnter = () => {
+            gsap.to([ref1.current], {
+                duration: 0.3,
+                translateY: -32,
+                ease: 'cubic.inOut',
+            });
+            gsap.to([ref2.current], {
+                duration: 0.3,
+                translateY: 0,
+                ease: 'cubic.inOut',
+            });
+        };
+
+        const handleMouseLeave = () => {
+            gsap.to([ref1.current, ref2.current], {
+                duration: 0.3,
+                translateY: 0,
+                ease: 'cubic.inOut',
+            });
+            gsap.to([ref2.current], {
+                duration: 0.3,
+                translateY: 32,
+                ease: 'cubic.inOut',
+            });
+        };
+
+        if (buttonRef.current) {
+            // @ts-ignore
+            buttonRef.current.addEventListener('mouseenter', handleMouseEnter);
+            // @ts-ignore
+            buttonRef.current.addEventListener('mouseleave', handleMouseLeave);
+        }
+
+        return () => {
+            // Cleanup function to remove event listeners
+            if (buttonRef.current) {
+                // @ts-ignore
+                buttonRef.current.removeEventListener('mouseenter', handleMouseEnter);
+                // @ts-ignore
+                buttonRef.current.removeEventListener('mouseleave', handleMouseLeave);
+            }
+        };
+    }, []);
+    return { ref1 , ref2 , buttonRef };
+}
